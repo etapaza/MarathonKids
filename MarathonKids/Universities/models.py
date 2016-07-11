bbe86f68aed66cf51f592a977e3811551c96ff08
Hurst <Luke.Hurst@nike.com>
2016-07-11 11:47:36 -0700

    Add Universities to settings and update database

--- MarathonKids/Universities/models.py
+++ MarathonKids/Universities/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class University(models.Model):
-	name = models.CharField(max_len = 100)
-	points = models.PositiveIntergerField()
-	password = models.CharField(max_len = 25)
+	name = models.CharField(max_length=100)
+	points = models.PositiveIntegerField()
+	password = models.CharField(max_length=25)
 
