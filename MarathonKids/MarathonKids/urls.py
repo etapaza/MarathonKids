2086bbd739e3c94d2bed938207f01ddfd5785297
Apaza <Elaine.Apaza@nike.com>
2016-07-19 09:19:09 -0700

    homepage

--- MarathonKids/MarathonKids/urls.py
+++ MarathonKids/MarathonKids/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^universities/', include("Universities.urls") ),
-    url(r'^$', views.index)
+    url(r'^$', views.index, name="MarathonKids-home")
 ]
