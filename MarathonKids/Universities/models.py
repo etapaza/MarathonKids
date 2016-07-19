2b77eb221009671bc7b5087c12b77f13f8476afd
Hurst <Luke.Hurst@nike.com>
2016-07-18 17:13:20 -0700

    Modify University model to include kids and miles
    Closes #7

--- MarathonKids/Universities/models.py
+++ MarathonKids/Universities/models.py
@@ -7,12 +7,13 @@ class Counter:
         self.count += 1
         return self.count
 
-
 # Create your models here.
 class University(models.Model):
 	name = models.CharField(max_length=100)
 	password = models.CharField(max_length=25)
 	domain = models.CharField(max_length=500, default="ncaa.org")
 	points = models.PositiveIntegerField()
+	kids_enrolled = models.PositiveIntegerField(default=0)
+	miles_ran = models.PositiveIntegerField(default=0)
 	
 
--- MarathonKids/startup.sql
+++ MarathonKids/startup.sql
@@ -1,5 +1,5 @@
-INSERT INTO Universities_university("id", "name", "password", "domain", "points") VALUES (0, "University of Michigan","goblue","umich.edu", 75);
-INSERT INTO Universities_university("id", "name", "password", "domain", "points") VALUES (1, "Michigan State University","sparty","msu.edu", 50);
-INSERT INTO Universities_university("id", "name", "password", "domain", "points") VALUES (2, "Ohio State University","buckeye","osu.edu", 60);
-INSERT INTO Universities_university("id", "name", "password", "domain", "points") VALUES (3, "Duke","bluedevils","duke.edu", 69);
-INSERT INTO Universities_university("id", "name", "password", "domain", "points") VALUES (4, "University of Oregon","ducks","uoregon.edu", 43);
+INSERT INTO Universities_university("id", "name", "password", "domain", "points", "kids_enrolled", "miles_ran") VALUES (0, "University of Michigan","goblue","umich.edu", 75, 0, 0);
+INSERT INTO Universities_university("id", "name", "password", "domain", "points", "kids_enrolled", "miles_ran") VALUES (1, "Michigan State University","sparty","msu.edu", 50, 0, 0);
+INSERT INTO Universities_university("id", "name", "password", "domain", "points", "kids_enrolled", "miles_ran") VALUES (2, "Ohio State University","buckeye","osu.edu", 60, 0, 0);
+INSERT INTO Universities_university("id", "name", "password", "domain", "points", "kids_enrolled", "miles_ran") VALUES (3, "Duke","bluedevils","duke.edu", 69, 0, 0);
+INSERT INTO Universities_university("id", "name", "password", "domain", "points", "kids_enrolled", "miles_ran") VALUES (4, "University of Oregon","ducks","uoregon.edu", 43, 0, 0);
