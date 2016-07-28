3720121e4550ad8f7289e8a08eee4c687691e7c2
Hurst <Luke.Hurst@nike.com>
2016-07-28 14:48:38 -0700

    Add University objects via create endpoint`

--- MarathonKids/Universities/views.py
+++ MarathonKids/Universities/views.py
@@ -2,11 +2,96 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Universities.models import University
 from django.contrib import auth
+from datetime import datetime
 
 # Create your views here.
 def profile(request, universityid):
 	university = University.objects.get(id=universityid)
 	return render(request, 'Universities\\profile.html', {'university': university})
+
+def create(request):
+	University.objects.all().delete()
+	University.objects.create_user(
+		id=0,
+		username='umich.edu',
+		password='goblue',
+		name='University of Michigan',
+		points=75,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=1,
+		username='msu.edu',
+		password='spartyon',
+		name='Michigan State University',
+		points='50',
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=2,
+		username='osu.edu',
+		password='buckeye',
+		name='Ohio State University',
+		points=60,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=3,
+		username='duke.edu',
+		password='bluedevil',
+		name='Duke University',
+		points=69,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=4,
+		username='uoregon.edu',
+		password='duck',
+		name='University of Oregon',
+		points=43,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	return redirect('MarathonKids-home')
 
 def login(request):
 	print('Received', request)
