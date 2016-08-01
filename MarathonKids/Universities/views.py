52c2e6dc3f26dbb53b864a2b04da1e987d435dc5
Elaine Apaza <elaine.apaza@nike.com>
2016-07-31 22:59:55 -0700

    stuff

--- MarathonKids/Universities/views.py
+++ MarathonKids/Universities/views.py
@@ -2,11 +2,101 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from Universities.models import University
 from django.contrib import auth
+from datetime import datetime
+
+def toOrdinalNum(n):
+	return str(n) + {1: 'st', 2: 'nd', 3: 'rd'}.get(4 if 10 <= n % 100 < 20 else n % 10, "th")
+
 
 # Create your views here.
 def profile(request, universityid):
 	university = University.objects.get(id=universityid)
-	return render(request, 'Universities\\profile.html', {'university': university})
+	rank = toOrdinalNum(University.objects.filter(points__gt = university.points).count() + 1)
+	return render(request, 'Universities\\profile.html', {'university': university, 'rank': rank[0:1], 'ord': rank[1:]})
+
+def create(request):
+	University.objects.all().delete()
+	University.objects.create_user(
+		id=0,
+		username='umich.edu',
+		password='goblue',
+		name='University of Michigan',
+		points=75,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=1,
+		username='msu.edu',
+		password='spartyon',
+		name='Michigan State University',
+		points='50',
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=2,
+		username='osu.edu',
+		password='buckeye',
+		name='Ohio State University',
+		points=60,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=3,
+		username='duke.edu',
+		password='bluedevil',
+		name='Duke University',
+		points=69,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	University.objects.create_user(
+		id=4,
+		username='uoregon.edu',
+		password='duck',
+		name='University of Oregon',
+		points=43,
+		kids_enrolled=0,
+		miles_ran=0,
+		date_joined=datetime.now(),
+		email='',
+		first_name='',
+		last_name='',
+		is_active=True,
+		is_staff=False,
+		is_superuser=False,
+	)
+	return redirect('MarathonKids-home')
 
 def login(request):
 	print('Received', request)
@@ -15,11 +105,19 @@ def login(request):
 	password = request.POST['password']
 	user = auth.authenticate(username=username, password=password)
 	auth.login(request, user)
-	return redirect('MarathonKids-home')
+	return redirect(request.META['HTTP_REFERER'])
 
 def logout(request):
-	print('LOL not logging out')
-	return redirect('MarathonKids-home')
+	auth.logout(request)
+	return redirect(request.META['HTTP_REFERER'])
 
-def edit(request):
-	return HttpResponse('Edit page')
+def update(request):
+	print('Updating:', request.user)
+	print(request.POST)
+	to_update = University.objects.get(id=request.user.id)
+	if request.POST.get('kids-value'):
+		to_update.kids_enrolled += int(request.POST['kids-value'])
+	else:
+		to_update.miles_ran += int(request.POST['miles-value'])
+	to_update.save()
+	return profile(request, request.user.id)
--- MarathonKids/create_users.py
+++
@@ -1,3 +0,0 @@
-from Universities.models import University
-
-University.objects.create_user(username=)
